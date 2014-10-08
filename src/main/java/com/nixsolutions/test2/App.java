fa69e050301ebc92953046077dd1b4fc3960c9f3
nizhnik <engelvamp@gmail.com>
2014-10-08 10:27:06 +0300

    Signed-off-by: nizhnik <engelvamp@gmail.com>

--- src/main/java/com/nixsolutions/test2/App.java
+++ src/main/java/com/nixsolutions/test2/App.java
@@ -8,7 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!1212112" );
-        System.out.println( "Hello World!1212112" );
+        System.out.println( "Hello!" );
     }
 }
